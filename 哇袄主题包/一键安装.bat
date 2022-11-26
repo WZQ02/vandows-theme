5c04dedd60588daed037a68d3d3fce8696dcc789
WZQ02 <ziqiwang2002@Hotmail.com>
2022-11-26 17:03:21 +0800

    额

--- 哇袄主题包/一键安装.bat
+++ 哇袄主题包/一键安装.bat
@@ -7,9 +7,7 @@ copy Web\Wallpaper\Windows %SystemDrive%\Windows\Web\Wallpaper\Windows
 
 "OTTO Theme for Windows 10 and 11 v1.theme"
 
-copy oemlogo.bmp %SystemDrive%\Windows\ottooem.bmp
 reg add HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation /v Manufacturer /t REG_SZ /d "¹þÄ·£¿¹þÄ·£¡¹þÄ·µÄ¹þ±´±´¹þÄ·£¡¹þÄ·£¿¹þÄ·£¡¹þÄ·µÄ¹þ±´±´¹þÄ·£¡¹þÄ·£¿¹þÄ·£¡¹þÄ·µÄ¹þ±´±´¹þÄ·£¡¹þÄ·£¿¹þÄ·£¡¹þÄ·µÄ¹þ±´±´¹þÄ·£¡" /f
 reg add HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation /v Model /t REG_SZ /d "BiSanLangAoLiAnFeiÎÔ²Û£¬±ù£¡£¡£¡" /f
 reg add HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation /v SupportURL /t REG_SZ /d "https://space.bilibili.com/1091064025" /f
-reg add HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation /v logo /t REG_SZ /d "%SystemDrive%/Windows/ottooem.bmp" /f
-reg add HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation /v SupportPhone /t REG_SZ /d "010" /f
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation /v SupportPhone /t REG_SZ /d "01012306" /f
